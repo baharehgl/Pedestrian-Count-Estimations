e4d5fe8f4c27f6142257d2446266488a0d69301e
baharehgl <88573876+baharehgl@users.noreply.github.com>
2025-11-24 15:02:49 -0800

    Update compare_R.py

--- compare_R.py
+++ compare_R.py
@@ -5,7 +5,7 @@
 # Train / Test split: holdout == 0 (train), holdout == 1 (test)
 # Data file: processed.csv
 # ============================================
-
+import os
 import numpy as np
 import pandas as pd
 
@@ -81,7 +81,9 @@ def mcfadden_pseudo_r2_poisson(y_true, y_pred, y_null_mean):
 # -------------------------
 
 # If you want to use the raw file instead, replace with: 'df1_v1a_out.csv'
-data_path = "processed.csv"
+#data_path = "processed.csv"
+PROCESSED_DIR = "./processed"
+data_path = os.path.join(PROCESSED_DIR, "processed.csv")
 df = pd.read_csv(data_path)
 
 # we assume these columns exist: 'pm_tot', 'holdout', and all feature columns
